cc8e89b20713e2991da6281f9e0e6358ad9fb768
Whats Up <whatsup@Whats-MacBook-Pro.local>
2020-02-27 11:29:06 +0100

    menu for mobile version

--- components/sections/BurgeMenu.js
+++ components/sections/BurgeMenu.js
@@ -11,18 +11,43 @@ const BurgerMenu = () => {
     });
   };
   return (
-    <div className="menuContainer">
-      <div>
-        <img src="static/logo.png" height={90} width={135} />
+    <div>
+      <div className="menuContainer">
+        <div>
+          <img src="static/logo.png" height={90} width={135} />
+        </div>
+        <HamburgerVortexReverse
+          isActive={isActive}
+          toggleButton={toggleButton}
+          buttonColor="transparent"
+          barColor="white"
+          style={{ position: "absolute", right: 0 }}
+        />
       </div>
-      <div></div>
-      <HamburgerVortexReverse
-        isActive={isActive}
-        toggleButton={toggleButton}
-        buttonColor="transparent"
-        barColor="white"
-        style={{ position: "absolute", right: 0 }}
-      />
+      {isActive && (
+        <div className="linksContainer">
+          <Link href="/">
+            <a>HOME</a>
+          </Link>
+          <Link href="news">
+            <a id="fifthChild">BLOG</a>
+          </Link>
+          <Link href="/#shopPage">
+            <a id="secondChild">
+              SHOP <i className="downArrow"></i>
+            </a>
+          </Link>
+          <Link href="/#aboutPage">
+            <a id="fourthChild">ABOUT</a>
+          </Link>
+          <Link href="/#whyusPage">
+            <a id="thirdChild">WHY US</a>
+          </Link>
+          <Link href="recommended">
+            <a id="sixthChild">RECOMMENDED</a>
+          </Link>
+        </div>
+      )}
 
       <style jsx>{`
         .menuContainer{
@@ -42,6 +67,31 @@ const BurgerMenu = () => {
                 flex-direction: row; 
                 justify-content: center;
                 align-items: center;
+            }
+            .linksContainer {
+                display: flex;
+
+                align-content: center;
+                align-items: center;
+                flex-direction: column;
+                background-color: #1C1713;
+                position: absolute;
+                top: 130px;
+                z-Index: 10;
+                width: 100%;
+                height: 100%;
+
+            }
+            .linksContainer > a {
+                text-decoration: none;
+                align: center;
+                width: 100px;
+                margin-top: 20px;
+                font-family: Ronnia, regular;
+                font-size: 22px;
+                font-weight: bold;
+                text-transform: capitalize;
+                color: #fafafabf;
             }
         `}</style>
     </div>
