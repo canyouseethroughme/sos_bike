ba12bc76338cdfddaf7dbe338168c8b7b3c61173
Whats Up <whatsup@whats-mbp.home>
2020-03-27 13:24:43 +0100

    coming soon page for web

--- pages/coming-soon.js
+++ pages/coming-soon.js
@@ -1,18 +1,35 @@
 import React from "react";
+import Navbar2 from "../components/sections/Navbar2";
+import Footer from "../components/sections/Footer";
 
 const ComingSoon = () => {
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        backgroundColor: "#997f67",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-    >
-      Coming Soon
+    <div>
+      <Navbar2 />
+      <div className="imgContainer">
+        <img src="static/signnwhite.png" width={200} height={200} />
+      </div>
+      <Footer />
+      <style jsx global>{`
+        * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+        }
+        body {
+          background-color: #1c1713;
+        }
+      `}</style>
+      <style jsx>{`
+        .imgContainer {
+          display: flex;
+          justify-content: center;
+          background-color: #1c1713;
+        }
+        .imgContainer > img {
+          margin-top: 200px;
+        }
+      `}</style>
     </div>
   );
 };
