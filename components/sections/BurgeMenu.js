2de4035d66bf4e0ab8975cfcd87ca4950d605bb2
Whats Up <whatsup@whats-mbp.home>
2020-05-11 18:00:29 +0200

    set page indicator on burger menu

--- components/sections/BurgeMenu.js
+++ components/sections/BurgeMenu.js
@@ -1,12 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { HamburgerVortexReverse } from "react-animated-burgers";
 
 const BurgerMenu = () => {
   const [isActive, setIsActive] = useState(false);
+  const [menuPosition, setMenuPostition] = useState("");
 
+  useEffect(() => {
+    // Update the document title using the browser API
+    const url = window.location.href.split("/")[3];
+    console.log(window.location.href.split("/"));
+    console.log("url here", url);
+    setMenuPostition(url);
+  }, []);
   const toggleButton = () => {
-    setIsActive(prevIsActive => {
+    setIsActive((prevIsActive) => {
       return !prevIsActive;
     });
   };
@@ -26,26 +34,56 @@ const BurgerMenu = () => {
       </div>
       {isActive && (
         <div className="linksContainer">
-          <Link href="/">
-            <a>HOME</a>
-          </Link>
-          <Link href="news">
-            <a id="fifthChild">BLOG</a>
-          </Link>
-          <Link href="/#shopPage">
-            <a id="secondChild">
-              SHOP <i className="downArrow"></i>
-            </a>
-          </Link>
-          <Link href="/#aboutPage">
-            <a id="fourthChild">ABOUT</a>
-          </Link>
-          <Link href="/#whyusPage">
-            <a id="thirdChild">WHY US</a>
-          </Link>
-          <Link href="recommended">
-            <a id="sixthChild">RECOMMENDED</a>
-          </Link>
+          <div className="linkContainer">
+            <div className="lineContainer">{menuPosition === ""}</div>
+            <Link href="/">
+              <a>HOME</a>
+            </Link>
+          </div>
+          <div className="linkContainer">
+            <div className="lineContainer">
+              {menuPosition === "news" && <div className="lineDiv"></div>}
+            </div>
+            <Link href="news">
+              <a id="fifthChild">BLOG</a>
+            </Link>
+          </div>
+          <div className="linkContainer">
+            <div className="lineContainer">
+              {menuPosition === "#shopPage" && <div className="lineDiv"></div>}
+            </div>
+            <Link href="/#shopPage">
+              <a id="secondChild">
+                SHOP <i className="downArrow"></i>
+              </a>
+            </Link>
+          </div>
+          <div className="linkContainer">
+            <div className="lineContainer">
+              {menuPosition === "#aboutPage" && <div className="lineDiv"></div>}
+            </div>
+            <Link href="/#aboutPage">
+              <a id="fourthChild">ABOUT</a>
+            </Link>
+          </div>
+          <div className="linkContainer">
+            <div className="lineContainer">
+              {menuPosition === "#whyusPage" && <div className="lineDiv"></div>}
+            </div>
+            <Link href="/#whyusPage">
+              <a id="thirdChild">WHY US</a>
+            </Link>
+          </div>
+          <div className="linkContainer">
+            <div className="lineContainer">
+              {menuPosition === "recommended" && (
+                <div className="lineDiv"></div>
+              )}
+            </div>
+            <Link href="recommended">
+              <a id="sixthChild">RECOMMENDED</a>
+            </Link>
+          </div>
         </div>
       )}
 
@@ -71,8 +109,7 @@ const BurgerMenu = () => {
             .linksContainer {
                 display: flex;
 
-                align-content: center;
-                align-items: center;
+                
                 flex-direction: column;
                 background-color: #1C1713;
                 position: absolute;
@@ -82,7 +119,7 @@ const BurgerMenu = () => {
                 height: 100%;
 
             }
-            .linksContainer > a {
+            .linksContainer > div >  a {
                 text-decoration: none;
                 align: center;
                 width: 100px;
@@ -92,6 +129,24 @@ const BurgerMenu = () => {
                 font-weight: bold;
                 text-transform: capitalize;
                 color: #fafafabf;
+            }
+
+            .linkContainer {
+              display: flex;
+              flex-direction: row;
+            }
+            .lineContainer{
+              width: 120px;
+              height: 100%;
+              display: flex;
+              align-items: center;
+            }
+            .lineDiv{
+              height: 3px;
+              width: 30px;
+              background-color: #997F67;
+              margin-top: 20px;
+              margin-left: 70px;
             }
         `}</style>
     </div>
